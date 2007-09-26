771e9490878af78fdd301904aebadcbf04626a85
Albert Astals Cid <tsdgeos@terra.es>
2007-09-26 18:46:24 +0000

    insert libkdegames catalog so that KGameDifficulty messages appear translated
    svn path=/trunk/KDE/kdegames/bovo/; revision=717363

--- gui/main.cc
+++ gui/main.cc
@@ -41,6 +41,7 @@ int main(int argc, char **argv) {
     KCmdLineArgs::init(argc, argv, &aboutData);
 
     KApplication application;
+    KGlobal::locale()->insertCatalog("libkdegames");
 
     if( application.isSessionRestored() ) {
         RESTORE(MainWindow);
