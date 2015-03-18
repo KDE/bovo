c9dfff9207e45c971fae2092fa94f8710a85e768
Montel Laurent <montel@kde.org>
2015-03-18 07:23:12 +0100

    Move Qapplication after kdelibs4support

--- gui/main.cc
+++ gui/main.cc
@@ -35,13 +35,13 @@ static const char copyleft[] = I18N_NOOP("(c) 2002-2007, Aron Boström");
 using namespace gui;
 
 int main(int argc, char **argv) {
-    QApplication app(argc, argv);
 
     Kdelibs4ConfigMigrator migrate(QStringLiteral("bovo"));
     migrate.setConfigFiles(QStringList() << QStringLiteral("bovorc"));
     migrate.setUiFiles(QStringList() << QStringLiteral("bovoui.rc"));
     migrate.migrate();
 
+    QApplication app(argc, argv);
     KLocalizedString::setApplicationDomain("bovo");
 
     KAboutData aboutData(QStringLiteral("bovo"), i18n("Bovo"),
