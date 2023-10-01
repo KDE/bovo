c1caff16e89b0a56fd11e8b77fb67d291f61c87e
Friedrich W. H. Kossebau <kossebau@kde.org>
2023-10-01 16:08:01 +0200

    Use more member function-pointer-based signal/slot connections
    GIT_SILENT

--- gui/mainwindow.cc
+++ gui/mainwindow.cc
@@ -183,8 +183,8 @@ void MainWindow::saveSettings() {
 }
 
 void MainWindow::setupActions() {
-    KStandardGameAction::gameNew(this, SLOT(slotNewGame()), actionCollection());
-    KStandardGameAction::quit(this, SLOT(close()), actionCollection());
+    KStandardGameAction::gameNew(this, &MainWindow::slotNewGame, actionCollection());
+    KStandardGameAction::quit(this, &MainWindow::close, actionCollection());
 
     auto replayAct = new QAction(QIcon::fromTheme( QStringLiteral( "media-playback-start" )),
                             i18n("&Replay"), this);
@@ -193,7 +193,7 @@ void MainWindow::setupActions() {
     replayAct->setWhatsThis(i18n("Replays your last game for you to watch."));
     replayAct->setEnabled(false);
 
-    m_hintAct = KStandardGameAction::hint(this, SLOT(hint()), actionCollection());
+    m_hintAct = KStandardGameAction::hint(this, &MainWindow::hint, actionCollection());
     m_hintAct->setEnabled(false);
 
     auto animAct = new KToggleAction(i18n("&Animation"),this);
@@ -217,9 +217,9 @@ void MainWindow::setupActions() {
     m_themeAct->setCurrentItem(themeId);
     actionCollection()->addAction( QStringLiteral( "themes" ), m_themeAct);
     m_themeAct->setIcon(QIcon::fromTheme( QStringLiteral( "games-config-theme" )));
-    connect(m_themeAct,SIGNAL(triggered(int)),this,SLOT(changeTheme(int)));
+    connect(m_themeAct, &KSelectAction::indexTriggered, this, &MainWindow::changeTheme);
 
-    m_undoAct = KStandardGameAction::undo(this, SLOT(slotUndo()), actionCollection());
+    m_undoAct = KStandardGameAction::undo(this, &MainWindow::slotUndo, actionCollection());
     m_undoAct->setEnabled(false);
 
     addAction(replayAct);
